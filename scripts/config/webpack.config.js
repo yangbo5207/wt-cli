8809d44e80baf7d54ce531b44d5a0afb7776dc0d
yangbo <yangbo@tigerbrokers.com>
2018-01-15 17:00:50 +0800

    update

--- scripts/config/webpack.config.js
+++ scripts/config/webpack.config.js
@@ -1,5 +1,3 @@
-// process.env.NODE_ENV = 'development';
-
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
